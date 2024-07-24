0fe8957da1821a6022eaf51b26e895e4d7bbfafc
GurTati <tatiana.gurova.j@gmail.com>
2024-07-24 19:10:55 +0100

    test V when 5

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-function roman_numerals(a, b) {
-  return a + b;
+function roman_numerals(a) {
+  if (a === 1) {
+    return 'I';
+  }
 }
 module.exports = roman_numerals;
--- app.test.js
+++ app.test.js
@@ -1,5 +1,10 @@
-const roman_numerals = require('./sum');
+const roman_numerals = require('./app');
 
-test("return I when 1", () => {
-  expect(roman_numerals(1)).toBe("I");
+// test("return I when 1", () => {
+//   expect(roman_numerals(1)).toBe("I");
+// });
+
+test("return V when 5", () => {
+  expect(roman_numerals(5)).toBe("V");
 });
+//your turn
